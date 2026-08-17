f37a8aa573c6f9ad8ba66c31e9dd4f1f14ee9f4c
thaina <l.rodrigues.thaina@gmail.com>
2026-08-17 14:13:36 -0300

    editar rotas

--- view/rotas.php
+++ view/rotas.php
@@ -4,7 +4,7 @@
         'home' => 'pagina-inicial.php',
         'categorias' => 'categoria/consultar.php',
         'add/categoria' => 'categoria/salvar.php',
-        'fornecedores' => 'fornecedores/consultar.php',
+        'fornecedores' => 'fornecedores/consultar.php', 
         'add/fornecedor' => 'fornecedores/salvar.php',
         'clientes' => 'clientes/consultar.php',
         'add/cliente' => 'clientes/salvar.php',
